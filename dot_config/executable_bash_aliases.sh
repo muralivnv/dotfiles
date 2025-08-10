faf03dee3a7f1988690b1a463516bec880e1f70f
muralivnv <muralivnv@gmail.com>
2025-08-10 12:26:03 -0700

    add bat config and move bash aliases to config

--- dot_config/executable_bash_aliases.sh
+++
@@ -1,9 +0,0 @@
-# setup custom aliases
-alias '..'='cd ..'
-alias gr="python3 $HOME/.config/scripts/git_repo_list.py"
-alias gl="python3 $HOME/.config/scripts/git_log.py"
-alias gc="python3 $HOME/.config/scripts/git_commit.py"
-alias gf="git fetch --all"
-alias gp="git pull --rebase"
-alias gP="git push"
-export EDITOR=hx
--- dot_config/executable_bash_config.sh
+++ dot_config/executable_bash_config.sh
@@ -1,6 +1,5 @@
 source ~/.config/bash_custom_functions.sh
 source ~/.config/fzf_tab/fzf-bash-completion.sh
-source ~/.config/bash_aliases.sh
 
 # set PATH so it includes user's private ~/.local/bin if it exists
 if [ -d "$HOME/.local/bin" ] ; then
@@ -20,3 +19,12 @@ bind -x '"\C-h": __fzf_history__'
 bind -x '"\C-o": __fzf_cd__'
 bind -x '"\C-p": fzf_file_widget'
 bind -x '"\t" : fzf_bash_completion'
+
+alias '..'='cd ..'
+alias gr="python3 $HOME/.config/scripts/git_repo_list.py"
+alias gl="python3 $HOME/.config/scripts/git_log.py"
+alias gc="python3 $HOME/.config/scripts/git_commit.py"
+alias gf="git fetch --all"
+alias gp="git pull --rebase"
+alias gP="git push"
+export EDITOR=hx
