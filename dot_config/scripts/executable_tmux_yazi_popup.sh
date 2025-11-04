06f01df7f82e8d62c3e79de0c950462c61364e64
muralivnv <muralivnv@gmail.com>
2025-11-03 20:08:40 -0800

    Improve yazi popup user-experience by using F5 to toggle yazi session popup

---
+++ dot_config/scripts/executable_tmux_yazi_popup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+SESSION="yazi_session"
+CWD="${1:-$PWD}"
+POPUP_HEIGHT="80%"
+POPUP_WIDTH="80%"
+
+if [ -z "$TMUX" ]; then
+  echo "Cannot open tmux popup outside tmux session." >&2
+  exit 1
+fi
+
+CURRENT_SESSION="$(tmux display-message -p '#S')"
+
+# if we are already attached to the yazi session, detach from session
+if [ "$CURRENT_SESSION" = "$SESSION" ]; then
+    tmux detach-client
+    exit 0
+fi
+
+if ! tmux has-session -t "$SESSION" 2>/dev/null; then
+    tmux new-session -ds "$SESSION" -c "$CWD"
+    tmux set -t "$SESSION" status off
+    tmux send-keys -t "$SESSION" "yazi" C-m
+fi
+
+tmux display-popup -d "$CWD" -h "$POPUP_HEIGHT" -w "$POPUP_WIDTH" -E "tmux attach -t '$SESSION'"
