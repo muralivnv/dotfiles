519a297301e1dab64b311f6540ab6939979bf60a
muralivnv <muralivnv@gmail.com>
2025-11-06 21:50:54 -0800

    build fully static version of tmux

--- .chezmoiscripts/run_once_install_tmux.sh
+++ .chezmoiscripts/run_once_install_tmux.sh
@@ -3,17 +3,45 @@ set -euo pipefail
 
 TMP_DIR="$(mktemp -d)"
 trap 'rm -rf "$TMP_DIR"' EXIT
+cd $TMP_DIR
+mkdir -p $TMP_DIR/libs
 
-TAR_URL="https://github.com/tmux/tmux/archive/449f255f3ef0167c6d226148cdaabac70686dde9.tar.gz"
-ARCHIVE="$TMP_DIR/tmux.tar.gz"
-echo "Downloading tmux from $TAR_URL..."
-curl -L -o "$ARCHIVE" "$TAR_URL"
+############
+# LIBEVENT #
+############
+wget -O libevent.tar.gz https://github.com/libevent/libevent/archive/refs/tags/release-2.1.12-stable.tar.gz
+tar xvzf libevent.tar.gz
+cd $(find . -maxdepth 1 -type d -name "libevent-*" -printf '%P\n')
 
-echo "Extracting to $TMP_DIR..."
-tar -xzf "$ARCHIVE" -C "$TMP_DIR"
+echo "Building libevent ..."
+./autogen.sh
+./configure --prefix=$TMP_DIR/libs --disable-shared
+make
+make install
+cd ..
 
-SRC_DIR=$(find "$TMP_DIR" -maxdepth 1 -type d -name "tmux-*")
-cd "$SRC_DIR"
+###########
+# NCURSES #
+###########
+wget -O ncurses.tar.gz http://ftp.gnu.org/gnu/ncurses/ncurses-6.5.tar.gz
+tar xvzf ncurses.tar.gz
+cd $(find . -maxdepth 1 -type d -name "ncurses-*" -printf '%P\n')
+
+echo "Building ncurses ..."
+./configure --prefix=$TMP_DIR/libs --with-default-terminfo-dir=/usr/share/terminfo --with-terminfo-dirs="/usr/share/terminfo"
+
+make libs
+make install.libs
+make install.includes
+
+cd ..
+
+########
+# TMUX #
+########
+wget -O tmux.tar.gz https://github.com/tmux/tmux/archive/449f255f3ef0167c6d226148cdaabac70686dde9.tar.gz
+tar xvzf tmux.tar.gz
+cd $(find . -maxdepth 1 -type d -name "tmux-*" -printf '%P\n')
 
 # patch for non-blocking popup
 curl -L https://patch-diff.githubusercontent.com/raw/tmux/tmux/pull/4379.diff -o 4379.diff
@@ -21,10 +49,7 @@ git apply 4379.diff
 
 echo "Building tmux ..."
 ./autogen.sh
-./configure
+./configure --prefix=$HOME/.local/ --enable-static CFLAGS="-I$TMP_DIR/libs/include -I$TMP_DIR/libs/include/ncurses" LDFLAGS="-L$TMP_DIR/libs/lib -L$TMP_DIR/libs/include/ncurses -L$TMP_DIR/libs/include" LIBEVENT_CFLAGS="-I$TMP_DIR/libs/include" LIBEVENT_LIBS="-L$TMP_DIR/libs/lib -levent"
 make
-
-echo "Installing tmux..."
-sudo make install
-
-echo "tmux installed successfully."
+make install
+echo "Tmux installation complete."
